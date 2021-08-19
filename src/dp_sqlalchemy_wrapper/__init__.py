7d90314a8c419d21fdece45cd70ee8ae7ddb2603
dataPuzzler <dataPuzzler@gmail.com>
2021-08-19 18:41:17 +0200

    Export package functionality via __init__.__all__ field

--- src/dp_sqlalchemy_wrapper/__init__.py
+++ src/dp_sqlalchemy_wrapper/__init__.py
@@ -1,1 +1,5 @@
-__all__ = []
+from .base_config import BaseConfig, BaseDBConfig
+from .base_db import BaseDB
+from .declarative_util import makeBase
+
+__all__ = [BaseConfig, BaseDBConfig, BaseDB, makeBase]
